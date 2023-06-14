3e47d7d873a1de58a1eeb41c1e483681e7139cb4
brikaniuka <136334320+brikaniuka@users.noreply.github.com>
2023-06-14 12:30:50 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,16 @@
+
+document.getElementById('menu').addEventListener('click', function()
+{
+
+document.getElementById('navega').classList.toggle('mostrar');
+
+
+});
+
+lightbox.option({
+    'wrapAround': true,
+    'albumLabel': "Imagen %1 de %2",
+    'fadeDuration': 2000,
+    'maxWidth': 1000,
+    'maxHeight': 1000
+})
